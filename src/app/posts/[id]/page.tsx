ad10379cd84c1163273a746d962430c583d9436e
jhs512 <jangka512@gmail.com>
2024-09-11 11:46:16 +0900

    work

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,8 +1,15 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons/faArrowRight";
 import { faArrowsToEye } from "@fortawesome/free-solid-svg-icons/faArrowsToEye";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
 export default function PostPage({ params }: { params: { id: string } }) {
+  const [date, setDate] = useState<Date | undefined>();
   const { id } = params;
 
   const post = {
@@ -11,11 +18,22 @@ export default function PostPage({ params }: { params: { id: string } }) {
   };
 
   return (
-    <div className="text-red-500 text-4xl">
+    <div className="text-4xl">
       <h1>{post.title}</h1>
       <p>{post.content}</p>
       <FontAwesomeIcon icon={faArrowRight} />
       <FontAwesomeIcon icon={faArrowsToEye} />
+      <Button variant="outline">Button</Button>
+
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-md border"
+      />
+
+      <hr />
+      {date?.toString()}
     </div>
   );
 }
