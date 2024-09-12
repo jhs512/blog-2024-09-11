6459ac05fd2106a8e703cbb2cc009c0d754817fc
jhs512 <jangka512@gmail.com>
2024-09-12 11:05:01 +0900

    work

---
+++ data/posts.ts
@@ -0,0 +1,37 @@
+type Post = { id: number; title: string; content: string };
+
+export const posts: { [key: number]: Post } = {
+  3: {
+    id: 3,
+    title: "제목 3",
+    content: "",
+  },
+  2: {
+    id: 2,
+    title: "제목 2",
+    content: "",
+  },
+  1: {
+    id: 1,
+    title: "제목 1",
+    content: "",
+  },
+};
+
+posts[3].content = `
+# 제목 3
+## 제목 3
+### 제목 3
+`.trim();
+
+posts[2].content = `
+# 제목 2
+## 제목 2
+### 제목 2
+`.trim();
+
+posts[1].content = `
+# 제목 1
+## 제목 1
+### 제목 1
+`.trim();
--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -7,33 +7,18 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons/faArrowRight";
 import { faArrowsToEye } from "@fortawesome/free-solid-svg-icons/faArrowsToEye";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
+import { posts } from "../../../../data/posts";
 
 export default function PostPage({ params }: { params: { id: string } }) {
   const [date, setDate] = useState<Date | undefined>();
-  const { id } = params;
+  const id = parseInt(params.id);
 
-  const post = {
-    title: `제목 ${id}`,
-    content: `이 글은 ${id}번째 글입니다.`,
-  };
+  const post = posts[id];
 
   return (
     <div className="text-4xl">
       <h1>{post.title}</h1>
       <p>{post.content}</p>
-      <FontAwesomeIcon icon={faArrowRight} />
-      <FontAwesomeIcon icon={faArrowsToEye} />
-      <Button variant="outline">Button</Button>
-
-      <LocaleCalendar
-        mode="single"
-        selected={date}
-        onSelect={setDate}
-        className="rounded-md border"
-      />
-
-      <hr />
-      {date?.toString()}
     </div>
   );
 }
